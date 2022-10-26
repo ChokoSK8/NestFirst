690c5f2071369172bc2511be320592969fa9ab12
ChokoSK8 <antoine.brunSK8@live.fr>
2022-10-26 11:54:15 +0200

    create rest API to continue

--- src/nestFirst.module.ts
+++ src/nestFirst.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { nestFirstCtrl } from './nestFirst.controller';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
 	controllers: [ nestFirstCtrl ],
-	imports : [ UserModule ],
+	imports : [ UserModule, AuthModule, BookmarkModule ],
 })
 
 export class nestFirstModule {};
